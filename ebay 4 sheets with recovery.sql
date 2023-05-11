0ffda3f43cb5fae41857f70b8ea36922bc366425
Scarlett R <122937589+Scarlett-Row@users.noreply.github.com>
2023-05-10 20:25:55 -0500

    Initial commit

---
+++ ebay 4 sheets with recovery.sql
@@ -0,0 +1,164 @@
+-- ATTEMPTING to merge 3 queries--
+--Year 2021 first
+SELECT 'Collectibles' AS category, '2021' AS year, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/6675.92)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/219.0)*100, 2), '0.##') AS percentage_of_items_sold, ROUND(SUM(cat_sales)/SUM(one_time_buyers), 2) AS average_revenue_per_buyer
+FROM May20_Apr21_Category_Sales
+WHERE category LIKE '%collectibles>%'
+UNION ALL
+SELECT 'Clothing' AS category, '2021' AS year, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/6675.92)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/219.0)*100, 2), '0.##') AS percentage_of_items_sold, ROUND(SUM(cat_sales)/SUM(one_time_buyers), 2) AS average_revenue_per_buyer
+FROM May20_Apr21_Category_Sales
+WHERE category LIKE '%clothing, Shoes & Accessories%'
+UNION ALL
+SELECT 'Toys' AS category, '2021' AS year, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/6675.92)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/219.0)*100, 2), '0.##') AS percentage_of_items_sold, ROUND(SUM(cat_sales)/SUM(one_time_buyers), 2) AS average_revenue_per_buyer
+FROM May20_Apr21_Category_Sales
+WHERE category LIKE '%toys & hobbies>%'
+UNION ALL
+SELECT 'Electronics' AS category, '2021' AS year, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/6675.92)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/219.0)*100, 2), '0.##') AS percentage_of_items_sold, ROUND(SUM(cat_sales)/SUM(one_time_buyers), 2) AS average_revenue_per_buyer
+FROM May20_Apr21_Category_Sales
+WHERE category LIKE '%Consumer Electronics%'
+UNION ALL
+SELECT 'Crafts' AS category, '2021' AS year, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/6675.92)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/219.0)*100, 2), '0.##') AS percentage_of_items_sold, ROUND(SUM(cat_sales)/SUM(one_time_buyers), 2) AS average_revenue_per_buyer
+FROM May20_Apr21_Category_Sales
+WHERE category LIKE '%craft%'
+UNION ALL
+SELECT 'Home' AS category, '2021' AS year, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/6675.92)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/219.0)*100, 2), '0.##') AS percentage_of_items_sold, ROUND(SUM(cat_sales)/SUM(one_time_buyers), 2) AS average_revenue_per_buyer
+FROM May20_Apr21_Category_Sales
+WHERE category LIKE '%home & garden>%'
+UNION ALL
+SELECT 'Dolls' AS category, '2021' AS year, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/6675.92)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/219.0)*100, 2), '0.##') AS percentage_of_items_sold, ROUND(SUM(cat_sales)/SUM(one_time_buyers), 2) AS average_revenue_per_buyer
+FROM May20_Apr21_Category_Sales
+WHERE category LIKE '%dolls%'
+UNION ALL
+SELECT 'Books' AS category, '2021' AS year, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/6675.92)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/219.0)*100, 2), '0.##') AS percentage_of_items_sold, ROUND(SUM(cat_sales)/SUM(one_time_buyers), 2) AS average_revenue_per_buyer
+FROM May20_Apr21_Category_Sales
+WHERE category LIKE '%books%'
+UNION ALL
+SELECT 'Sporting' AS category, '2021' AS year, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/6675.92)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/219.0)*100, 2), '0.##') AS percentage_of_items_sold, ROUND(SUM(cat_sales)/SUM(one_time_buyers), 2) AS average_revenue_per_buyer
+FROM May20_Apr21_Category_Sales
+WHERE category LIKE '%sport%'
+ORDER BY one_time_buyers DESC
+
+UNION ALL --year 2022 follows
+
+SELECT 'Collectibles' AS category, '2022' AS year, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/6675.92)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/219.0)*100, 2), '0.##') AS percentage_of_items_sold, ROUND(SUM(cat_sales)/SUM(one_time_buyers), 2) AS average_revenue_per_buyer
+FROM May21_Apr22_Category_Sales
+WHERE category LIKE '%collectibles>%'
+UNION ALL
+SELECT 'Clothing' AS category, '2022' AS year, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/6675.92)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/219.0)*100, 2), '0.##') AS percentage_of_items_sold, ROUND(SUM(cat_sales)/SUM(one_time_buyers), 2) AS average_revenue_per_buyer
+FROM May21_Apr22_Category_Sales
+WHERE category LIKE '%clothing, Shoes & Accessories%'
+UNION ALL
+SELECT 'Toys' AS category, '2022' AS year, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/6675.92)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/219.0)*100, 2), '0.##') AS percentage_of_items_sold, ROUND(SUM(cat_sales)/SUM(one_time_buyers), 2) AS average_revenue_per_buyer
+FROM May21_Apr22_Category_Sales
+WHERE category LIKE '%toys & hobbies>%'
+UNION ALL
+SELECT 'Electronics' AS category, '2022' AS year, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/6675.92)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/219.0)*100, 2), '0.##') AS percentage_of_items_sold, ROUND(SUM(cat_sales)/SUM(one_time_buyers), 2) AS average_revenue_per_buyer
+FROM May21_Apr22_Category_Sales
+WHERE category LIKE '%Consumer Electronics%'
+UNION ALL
+SELECT 'Crafts' AS category, '2022' AS year, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/6675.92)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/219.0)*100, 2), '0.##') AS percentage_of_items_sold, ROUND(SUM(cat_sales)/SUM(one_time_buyers), 2) AS average_revenue_per_buyer
+FROM May21_Apr22_Category_Sales
+WHERE category LIKE '%craft%'
+UNION ALL
+SELECT 'Home' AS category, '2022' AS year, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/6675.92)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/219.0)*100, 2), '0.##') AS percentage_of_items_sold, ROUND(SUM(cat_sales)/SUM(one_time_buyers), 2) AS average_revenue_per_buyer
+FROM May21_Apr22_Category_Sales
+WHERE category LIKE '%home & garden>%'
+UNION ALL
+SELECT 'Dolls' AS category, '2022' AS year, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/6675.92)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/219.0)*100, 2), '0.##') AS percentage_of_items_sold, ROUND(SUM(cat_sales)/SUM(one_time_buyers), 2) AS average_revenue_per_buyer
+FROM May21_Apr22_Category_Sales
+WHERE category LIKE '%dolls%'
+UNION ALL
+SELECT 'Books' AS category, '2022' AS year, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/6675.92)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/219.0)*100, 2), '0.##') AS percentage_of_items_sold, ROUND(SUM(cat_sales)/SUM(one_time_buyers), 2) AS average_revenue_per_buyer
+FROM May21_Apr22_Category_Sales
+WHERE category LIKE '%books%'
+UNION ALL
+SELECT 'Sporting' AS category, '2022' AS year, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/6675.92)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/219.0)*100, 2), '0.##') AS percentage_of_items_sold, ROUND(SUM(cat_sales)/SUM(one_time_buyers), 2) AS average_revenue_per_buyer
+FROM May21_Apr22_Category_Sales
+WHERE category LIKE '%sport%'
+ORDER BY one_time_buyers DESC;
+
+--UNION ALL and PERIOD but with no simplified categories
+SELECT 'May22-Apr23' AS period, category, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers
+FROM May22_Apr23_Category_Sales
+WHERE category LIKE '%collectibles>%' OR category LIKE '%clothing, Shoes & Accessories%' OR category LIKE '%toys & hobbies>%' OR category LIKE '%Consumer Electronics%' OR category LIKE '%craft%' OR category LIKE '%home & garden>%' OR category LIKE '%dolls%' OR category LIKE '%books%' OR category LIKE '%sport%'
+GROUP BY category
+UNION ALL
+SELECT 'May21-Apr22' AS period, category, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers
+FROM May21_Apr22_Category_Sales
+WHERE category LIKE '%collectibles>%' OR category LIKE '%clothing, Shoes & Accessories%' OR category LIKE '%toys & hobbies>%' OR category LIKE '%Consumer Electronics%' OR category LIKE '%craft%' OR category LIKE '%home & garden>%' OR category LIKE '%dolls%' OR category LIKE '%books%' OR category LIKE '%sport%'
+GROUP BY category
+UNION ALL
+SELECT 'May20-Apr21' AS period, category, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers
+FROM May20_Apr21_Category_Sales
+WHERE category LIKE '%collectibles>%' OR category LIKE '%clothing, Shoes & Accessories%' OR category LIKE '%toys & hobbies>%' OR category LIKE '%Consumer Electronics%' OR category LIKE '%craft%' OR category LIKE '%home & garden>%' OR category LIKE '%dolls%' OR category LIKE '%books%' OR category LIKE '%sport%'
+GROUP BY category
+ORDER BY cat_sales DESC;
+
+
+
+--Using a CASE statement
+SELECT 
+    (CASE
+        WHEN category LIKE '%collectibles>%' THEN 'Collectibles'
+        WHEN category LIKE '%clothing, Shoes & Accessories%' THEN 'Clothing'
+        WHEN category LIKE '%toys & hobbies>%' THEN 'Toys'
+        WHEN category LIKE '%Consumer Electronics%' THEN 'Electronics'
+        WHEN category LIKE '%craft%' THEN 'Crafts'
+        WHEN category LIKE '%home & garden>%' THEN 'Home'
+        WHEN category LIKE '%dolls%' THEN 'Dolls'
+        WHEN category LIKE '%books%' THEN 'Books'
+    END AS category, 
+    '2021' AS year, 
+    SUM(cat_sales) AS cat_sales, 
+    SUM(one_time_buyers) AS one_time_buyers, 
+    FORMAT(ROUND((SUM(cat_sales)/6675.92)*100, 2), '0.##') AS percentage_of_total_sales, 
+    FORMAT(ROUND((SUM(one_time_buyers)/219.0)*100, 2), '0.##') AS percentage_of_items_sold, 
+    ROUND(SUM(cat_sales)/SUM(one_time_buyers), 2) AS average_revenue_per_buyer
+FROM [dbo].[May20_Apr21_Category_Sales]
+WHERE category IN (
+    '%collectibles>%',
+    '%clothing, Shoes & Accessories%',
+    '%toys & hobbies>%',
+    '%Consumer Electronics%',
+    '%craft%',
+    '%home & garden>%',
+    '%dolls%',
+    '%books%'
+)
+GROUP BY category;
+
+SELECT 'Collectibles' AS category, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/5592.38)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/197.0)*100, 2), '0.##') AS percentage_of_items_sold
+FROM May22_Apr23_Category_Sales
+WHERE category LIKE '%collectibles>%'
+UNION ALL
+SELECT 'Clothing' AS category, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/5592.38)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/197.0)*100, 2), '0.##') AS percentage_of_items_sold
+FROM May22_Apr23_Category_Sales
+WHERE category LIKE '%clothing, Shoes & Accessories%'
+UNION ALL
+SELECT 'Toys' AS category, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/5592.38)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/197.0)*100, 2), '0.##') AS percentage_of_items_sold
+FROM May22_Apr23_Category_Sales
+WHERE category LIKE '%toys & hobbies>%'
+UNION ALL
+SELECT 'Electronics' AS category, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/5592.38)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/197.0)*100, 2), '0.##') AS percentage_of_items_sold
+FROM May22_Apr23_Category_Sales
+WHERE category LIKE '%Consumer Electronics%'
+UNION ALL
+SELECT 'Crafts' AS category, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/5592.38)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/197.0)*100, 2), '0.##') AS percentage_of_items_sold
+FROM May22_Apr23_Category_Sales
+WHERE category LIKE '%craft%'
+UNION ALL
+SELECT 'Home' AS category, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/5592.38)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/197.0)*100, 2), '0.##') AS percentage_of_items_sold
+FROM May22_Apr23_Category_Sales
+WHERE category LIKE '%home & garden>%'
+UNION ALL
+SELECT 'Dolls' AS category, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/5592.38)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/197.0)*100, 2), '0.##') AS percentage_of_items_sold
+FROM May22_Apr23_Category_Sales
+WHERE category LIKE '%dolls%'
+UNION ALL
+SELECT 'Books' AS category, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/5592.38)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/197.0)*100, 2), '0.##') AS percentage_of_items_sold
+FROM May22_Apr23_Category_Sales
+WHERE category LIKE '%books%'
+UNION ALL
+SELECT 'Sporting' AS category, SUM(cat_sales) AS cat_sales, SUM(one_time_buyers) AS one_time_buyers, FORMAT(ROUND((SUM(cat_sales)/5592.38)*100, 2), '0.##') AS percentage_of_total_sales, FORMAT(ROUND((SUM(one_time_buyers)/197.0)*100, 2), '0.##') AS percentage_of_items_sold
+FROM May22_Apr23_Category_Sales
+WHERE category LIKE '%sport%'
+ORDER BY cat_sales DESC;
